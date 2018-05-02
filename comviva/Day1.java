0fd662386b389b3fcb598a2ab8d382746650c489
Naveen <adith.naveen@gmail.com>
2018-05-02 16:06:27 +0530

    adding i18n, db

--- comviva/Day1.java
+++ comviva/Day1.java
@@ -835,3 +835,476 @@ public class ClientApp {
    	
 </beans>
 
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
+Message Resource to show working of Internationalization 
+----------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
+package com.naveen05.messagesource;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
+public class HelloWorld {
+	private String greeting;
+	private String message;
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	public String getGreeting() {
+		return greeting;
+	}
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void sayHelloWorld(){
+		System.out.println(messageSource.getMessage("helloworld.greet", new Object[]{greeting, message},
+				"Good Morning...", null) +", ");		
+	}
+	
+	public void sayHelloWorld_fr(){
+		System.out.println(messageSource.getMessage("helloworld.greet", new Object[]{greeting, message},
+				"Good Morning...",  new Locale("FR")) +", ");		
+	}
+}
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:context="http://www.springframework.org/schema/context"
+    xsi:schemaLocation="http://www.springframework.org/schema/beans
+    http://www.springframework.org/schema/beans/spring-beans-3.0.xsd
+    http://www.springframework.org/schema/context
+    http://www.springframework.org/schema/context/spring-context-3.0.xsd">
+
+
+	<context:annotation-config/>
+	
+	<!--  by default every bean is the singleton, if you want the bean to be 
+		create every time you can pass it as scope="prototype" in bean tag -->
+	<bean id="helloWorld" class="com.naveen05.messagesource.HelloWorld">
+		<property name="greeting" value="Naveen"/>
+		<property name="message" value="How are you today?"/>
+		
+	</bean>	   	
+	   	
+	<bean id="messageSource" class="org.springframework.context.support.ResourceBundleMessageSource">
+		<property name="basenames">
+			<list>
+				<!--  no need to give .properties -->
+				<value>mymessages</value>
+			</list>
+		</property>
+	</bean>
+</beans>
+----------------------------------------------------------------------------------------------------------------------
+mymessages.properties
+----------------------------------------------------------------------------------------------------------------------
+goodmorning=shubhodaya
+helloworld.greet=({0}, Hope you are doing well, and i wanted to say {1})
+
+----------------------------------------------------------------------------------------------------------------------
+mymessages_fr.properties
+----------------------------------------------------------------------------------------------------------------------
+goodmorning=BonJour
+helloworld.greet=({0}, comment ca va {1})
+
+----------------------------------------------------------------------------------------------------------------------
+package com.naveen05.messagesource;
+
+import java.util.Locale;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("messageSource.xml");
+		System.out.println(context.getMessage("goodmorning", new Object[]{"Naveen", "kumar"},   new Locale("FR")));
+
+		System.out.println("----------------------------------------------------------------------");
+		System.out.println(context.getMessage("goodmorning", new Object[]{"Naveen", "kumar"}, "Good Morning in English", null));
+		
+		System.out.println("----------------------------------------------------------------------");
+		
+		HelloWorld hw = (HelloWorld) context.getBean("helloWorld");
+		hw.sayHelloWorld();
+		hw.sayHelloWorld_fr();
+	}
+}
+
+----------------------------------------------------------------------------------------------------------------------
+
+
+----------------------------------------------------------------------------------------------------------------------
+
+Spring DB
+----------------------------------------------------------------------------------------------------------------------
+
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://www.springframework.org/schema/beans 
+	   http://www.springframework.org/schema/beans/spring-beans.xsd">
+
+	<bean id="dataSource"
+		class="org.springframework.jdbc.datasource.DriverManagerDataSource">
+		<property name="driverClassName" value="oracle.jdbc.driver.OracleDriver" />
+		<property name="url" value="jdbc:oracle:thin:@10.0.0.16:1521:xe" />
+		<property name="username" value="naveen" />
+		<property name="password" value="naveen" />
+	</bean>
+
+   <!-- Definition for studentJDBCTemplate bean -->
+	<bean id="studentJDBCTemplate" 
+      class="com.naveen.dao.StudentJDBCTemplate">
+      <property name="dataSource"  ref="dataSource" />    
+   </bean>
+      
+</beans>
+
+	   					   
+
+_________________________________________________________
+Student DAO.java
+_________________________________________________________
+package com.naveen.interfaces;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.naveen.entity.Student;
+
+public interface StudentDAO {
+
+	/** 
+	 * to get student object from procedure
+	 * 
+	 */
+	public Student getStudentFromProcedure(int id);
+
+	/**
+	 * This is the method to be used to initialize database resources ie.
+	 * connection.
+	 */
+	public void setDataSource(DataSource ds);
+
+	/**
+	 * This is the method to be used to create a record in the Student table.
+	 */
+	public void create(int id, String name, Integer age);
+
+	/**
+	 * This is the method to be used to list down a record from the Student
+	 * table corresponding to a passed student id.
+	 */
+	public Student getStudent(Integer id);
+
+	/**
+	 * This is the method to be used to list down all the records from the
+	 * Student table.
+	 */
+	public List<Student> listStudents();
+
+	/**
+	 * This is the method to be used to delete a record from the Student table
+	 * corresponding to a passed student id.
+	 */
+	public void delete(Integer id);
+
+	/**
+	 * This is the method to be used to update a record into the Student table.
+	 */
+	public void update(Integer id, Integer age);
+}
+
+
+
+_______________________________________________
+Student.java
+_________________________________________________
+package com.naveen.entity;
+
+public class Student {
+	private Integer age;
+	private String name;
+	private Integer id;
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [age=" + age + ", name=" + name + ", id=" + id + "]";
+	}
+
+}
+
+
+
+
+--------------------------------------------------------
+StudentMApper.java
+______________________________________________________
+package com.naveen.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.naveen.entity.Student;
+
+public class StudentMapper implements RowMapper<Student> {
+	public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Student student = new Student();
+		student.setId(rs.getInt("id"));
+		student.setName(rs.getString("name"));
+		student.setAge(rs.getInt("age"));
+		return student;
+	}
+}
+
+
+_________________________________________________________
+StudentJDBCTemplate.java
+_________________________________________________________
+package com.naveen.dao;
+
+import java.sql.Types;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.jdbc.object.StoredProcedure;
+
+import com.naveen.entity.Student;
+import com.naveen.interfaces.StudentDAO;
+
+public class StudentJDBCTemplate implements StudentDAO {
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplateObject;
+	private SimpleJdbcCall simpleJdbcCall;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+
+	}
+
+	private class StudentSP extends StoredProcedure {
+		private static final String SPROC_NAME = "PR_GETRECORD";
+
+		public StudentSP(DataSource datasource) {
+			super(datasource, SPROC_NAME);
+			declareParameter(new SqlParameter("pr_id", Types.INTEGER)); // declaring
+																		// sql
+																		// in
+																		// parameter
+																		// to
+																		// pass
+																		// input
+			declareParameter(new SqlOutParameter("pr_name", Types.VARCHAR));
+			declareParameter(new SqlOutParameter("pr_age", Types.INTEGER));
+			
+			// declaring
+						
+		}
+
+		public Student execute(int sid) {
+			Map<String, Object> results = super.execute(sid);
+			
+			Student student = new Student();
+			student.setId(sid);
+			student.setName(results.get("pr_name").toString());
+			student.setAge(Integer.parseInt(results.get("pr_age").toString()));
+			
+			
+			return student; 	
+		}
+	}
+
+	public Student getStudentFromProcedure(int id) {
+		
+		return new StudentSP(dataSource).execute(102);
+		
+	}
+
+	public void create(int id, String name, Integer age) {
+		String SQL = "insert into Student (id, name, age) values (?, ?, ?)";
+
+		jdbcTemplateObject.update(SQL, id, name, age);
+		System.out.println("Created Record Name = " + name + " Age = " + age);
+		return;
+	}
+
+	public Student getStudent(Integer id) {
+		String SQL = "select * from Student where id = ?";
+		//Student student = jdbcTemplateObject.queryForObject(SQL, new Object[] { id },
+		//		new BeanPropertyRowMapper<Student>(Student.class));
+		
+		
+		System.out.println("using student mapper ");
+		return jdbcTemplateObject.queryForObject(SQL, new Object[]{id}, new StudentMapper());
+		
+		
+		// return student;
+	}
+	
+
+
+
+
+	public int getNumberOfStudents(){
+		String sql = "SELECT COUNT(*) FROM student";
+		 
+		
+		int total = jdbcTemplateObject.queryForInt(sql);
+	 
+		return total;
+
+	}
+
+
+
+
+
+
+	public List<Student> listStudents() {
+		String SQL = "select * from Student";
+
+		// first way
+		List<Student> studs = jdbcTemplateObject.query(SQL, new BeanPropertyRowMapper<Student>(Student.class));
+
+		// second way
+		/*
+		 * List<Student> students = new ArrayList<Student>(); List<Map<String,
+		 * Object>> rows = jdbcTemplateObject.queryForList(SQL);
+		 * 
+		 * for(Map row : rows){ Student temp = new Student();
+		 * temp.setId(Integer.parseInt(row.get("id").toString()));
+		 * temp.setName((String)row.get("name"));
+		 * temp.setAge(Integer.parseInt(row.get("age").toString()));
+		 * 
+		 * students.add(temp); }
+		 */
+
+		return studs;
+	}
+
+	public void delete(Integer id) {
+		String SQL = "delete from Student where id = ?";
+		jdbcTemplateObject.update(SQL, id);
+		System.out.println("Deleted Record with ID = " + id);
+		return;
+	}
+
+	public void update(Integer id, Integer age) {
+		String SQL = "update Student set age = ? where id = ?";
+		jdbcTemplateObject.update(SQL, age, id);
+		System.out.println("Updated Record with ID = " + id);
+		return;
+	}
+
+}
+
+
+
+_____________________________________________________
+Client.java
+____________________________________________________
+
+package com.naveen.client;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.naveen.dao.StudentJDBCTemplate;
+import com.naveen.entity.Student;
+
+public class Client {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("springdb.xml");
+
+		StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate) context.getBean("studentJDBCTemplate");
+
+		/** insert  */ 
+		// System.out.println("------Records Creation--------");
+		// studentJDBCTemplate.create(101, "Zara", 11);
+		// studentJDBCTemplate.create(102, "Nuha", 2);
+		// studentJDBCTemplate.create(103, "Ayan", 15);
+
+		// studentJDBCTemplate.delete(101);
+
+		// studentJDBCTemplate.update(102, 22);
+
+		// System.out.println(studentJDBCTemplate.getStudent(102));
+/*
+		System.out.println("------Listing Multiple Records--------");
+		List<Student> students = studentJDBCTemplate.listStudents();
+		for (Student record : students) {
+			System.out.print("ID : " + record.getId());
+			System.out.print(", Name : " + record.getName());
+			System.out.println(", Age : " + record.getAge());
+		}
+
+	*/	
+		System.out.println("+++++++++++++++++++++++++++");
+		
+		System.out.println(studentJDBCTemplate.getStudentFromProcedure(102));
+		
+	}
+}
+
+
+
+
